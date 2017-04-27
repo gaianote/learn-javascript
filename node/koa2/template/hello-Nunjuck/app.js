07373d6d0d326ba7dd24365307ae7fe52adbca7d
unknown <>
2017-04-27 22:44:48 +0800

    update

---
+++ node/koa2/template/hello-Nunjuck/app.js
@@ -0,0 +1,10 @@
+const Koa = require('koa');
+const app = new Koa();
+
+const nunjucks = require('nunjucks');
+nunjucks.configure('views', { autoescape: true });
+
+app.use(ctx => {
+  ctx.body = nunjucks.render('hello.html', { name: 'nunjucks' })
+});
+app.listen(3000);
